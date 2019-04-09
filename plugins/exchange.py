4f2f95c041ef9dade189e32253b786048c6e3dd3
Munkyo.Seo <munkyo.seo@trustus.co.kr>
2019-04-09 11:22:40 +0900

    환율정보 API 서버 변경

--- plugins/exchange.py
+++ plugins/exchange.py
@@ -5,29 +5,30 @@ from urllib.request import urlopen
 import bs4, json
 from plugins import key_config
 
-# https://ko.wikipedia.org/wiki/ISO_4217
 currency_code = {
-    '중국': 'CNY',
-    '한국': 'KRW',
-    '일본': 'JPY',
+    '중국': 'CNH',
+    '일본': 'JPY(100)',
     '홍콩': 'HKD',
     '미국': 'USD',
-    '베트남': 'VND',
-    '대만': 'TWD',
     '싱가포르': 'SGD',
     '싱가폴': 'SGD'
 }
 
 def run(currency):
-    key = "%s_KRW" % (currency_code[currency])
-    url = "https://free.currencyconverterapi.com/api/v6/convert?q=%s&compact=ultra&apiKey=%s" % \
-          (key, key_config.CURRENCYCONVERTERAPI)
-
+    key = currency_code[currency]
+    url = "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON?authkey=%s&searchdate=&data=AP01" % \
+          (key_config.CURRENCY_EXCHANGE_API_KEY)
+    print(url)
     try:
         with urlopen(url) as response:
             res = response.read()
             r = json.loads(res)
-            body_res = "[환율] **%s** => 한국: **%s** KRW" % (currency, r[key])
+            for result in r:
+                print(result)
+                if result['cur_unit'] == key:
+                    break
+            body_res = "**%s %s**\n팔때: %s KRW\n살때 %s KRW\n매매 기준: %s KRW" % \
+                       (currency, key, result['ttb'], result['tts'], result['deal_bas_r'])
     except Exception as e:
         body_res = "서버 오류로 응답할 수 없습니다."
         print(e)
--- plugins/key_config.sample.py
+++ plugins/key_config.sample.py
@@ -1,4 +1,5 @@
 #-*- coding: utf-8 -*-
 
-# Example: https://free.currencyconverterapi.com/api/v6/convert?q=USD_PHP&compact=ultra&apiKey={APIKEY}
-CURRENCYCONVERTERAPI = "YOUR_API_KEY"
+# https://www.koreaexim.go.kr/site/program/openapi/openApiView?menuid=001003002002001&apino=2&viewtype=C
+# https://www.koreaexim.go.kr/site/program/financial/exchangeJSON
+CURRENCY_EXCHANGE_API_KEY = "YOUR KOREAEXIM API KEY"
