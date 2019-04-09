dfd98e26129c9429872888d54c87c40b8d411583
Munkyo.Seo <munkyo.seo@trustus.co.kr>
2019-04-09 11:54:18 +0900

    쓸데없는 좋아요 기능

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask
 from flask_restful import Resource, Api, abort, reqparse
 from plugins import weather, exchange
 
+import random
+
 app = Flask(__name__)
 api = Api(app)
 
@@ -31,6 +33,8 @@ class Jandi(Resource):
         elif command == '환율':
             res = exchange.run(text.split(' ')[1])
 
+        if '좋지' in text or '좋아' in text or '좋니' in text:
+            res = random.choice(["네 좋아요!", "글쎄요 잘 모르겠어요", "당연하죠!", "사랑합니다.", "걍 일이나하죠?", "..."])
         return res, 200, headers
 
 api.add_resource(Jandi, '/')
